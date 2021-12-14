761d085505a7a5b40e9714b84b0326824b94169d
Moment <momincong@foxmail.com>
2021-12-14 09:44:22 +0800

    update method：状态压缩----最长回文子序列LongestPalindromicSubsequence516

--- src/leetcode/editor/cn/LongestPalindromicSubsequence516.java
+++ src/leetcode/editor/cn/LongestPalindromicSubsequence516.java
@@ -71,6 +71,31 @@ public class LongestPalindromicSubsequence516 {
             return dp[0][length - 1];
         }
     }
+
+    //状态压缩，搞不懂
+    class Solution2 {
+        public int longestPalindromeSubseq(String s) {
+            int length = s.length();
+            int[] dp = new int[length];
+            for (int i = 0; i < length; i++) dp[i] = 1;
+            //从下往上
+            for (int i = length - 2; i >= 0; i--) {
+                int pre = 0;
+                //从i往右
+                for (int j = i+1; j < length; j++) {
+                    int temp = dp[j];
+                    char leftChar = s.charAt(i);
+                    char rightChar = s.charAt(j);
+                    if (leftChar == rightChar)
+                        dp[j] = pre + 2;
+                    else
+                        dp[j] = Math.max(dp[j], dp[j - 1]);
+                    pre = temp;
+                }
+            }
+            return dp[length - 1];
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
