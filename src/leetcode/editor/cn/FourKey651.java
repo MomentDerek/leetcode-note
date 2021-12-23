eec18519e5985d982b696a57054af98ea612bdb6
Moment <momincong@foxmail.com>
2021-12-23 19:28:58 +0800

    update code 4键键盘FourKey651

--- src/leetcode/editor/cn/FourKey651.java
+++ src/leetcode/editor/cn/FourKey651.java
@@ -27,13 +27,16 @@ package leetcode.editor.cn;
 // 四键键盘
 public class FourKey651 {
     public static void main(String[] args) {
-        Solution solution = new FourKey651().new Solution();
+        Solution2 solution = new FourKey651().new Solution2();
         long startTime = System.currentTimeMillis();
+        int res = solution.maxA(7);
         long endTime = System.currentTimeMillis();
         double time = endTime - startTime;
+        System.out.println(res);
         System.out.println("程序运行时间： " + ((time > 10000) ? time / 1000 + "s" : time + "ms"));
     }
 
+    //效率高
     class Solution {
         public int maxA(int N) {
             if (N == 1) return 1;
@@ -50,6 +53,35 @@ public class FourKey651 {
             return dp[N];
         }
     }
+
+    //原始算法
+    class Solution2 {
+        public int maxA(int N) {
+            return dp(N, 0, 0);
+        }
+
+        /**
+         * @param n 当前剩下的操作步数
+         * @param copy 剪切板中的字符数
+         * @param now 当前输出的字符数
+         */
+        private int dp(int n, int copy, int now) {
+            if (n<=0) return now;
+            return
+                    //复制到剪切板
+                    max(dp(n - 2, now, now),
+                    //复制到输出
+                    dp(n - 1, copy, now + copy),
+                    //打印一个字符
+                    dp(n - 1, copy, now + 1));
+
+        }
+
+        private int max(int a, int b, int c) {
+            return Math.max(a, Math.max(b, c));
+        }
+    }
+
 
 }
 
